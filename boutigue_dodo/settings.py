345df53bc6ea448d5b1cf7c534a92928b3839f71
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-11-18 10:19:46 +0000

    Fixing issues with css

--- boutigue_dodo/settings.py
+++ boutigue_dodo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3w3vbu9rv#^mx0qvimmbvx9-(03=h_rk&3yp5w9i&fd9_!!kqk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition

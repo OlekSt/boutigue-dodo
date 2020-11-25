3371b5c23f901adbfee331c8724c473bca72c79b
Oleksiy Statsenko <alexey.statsenko@gmail.com>
2020-11-25 07:06:01 +0000

    Set secret key and debug mode

--- boutigue_dodo/settings.py
+++ boutigue_dodo/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3w3vbu9rv#^mx0qvimmbvx9-(03=h_rk&3yp5w9i&fd9_!!kqk'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutigue-dodo.herokuapp.com', 'localhost']
 
